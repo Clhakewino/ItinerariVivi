639286f0cad2ed9d575759778bd4a42b946823e1
Bruyin <chlacchy96@gmail.com>
2026-03-26 13:38:33 +0100

    aggiunte tutte le città in sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,18 +1,27 @@
 import { MetadataRoute } from 'next'
+import { ITINERARI_FULL } from './data/itinerari';
 
-export default function sitemap(): MetadataRoute.Sitemap {
-  return [
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const baseUrl = 'https://itinerari-vivi.vercel.app';
+
+  // rotte statiche (Home, Chi Siamo, ecc.)
+  const staticRoutes = [
     {
-      url: 'https://itinerari-vivi.vercel.app',
+      url: baseUrl,
       lastModified: new Date(),
-      changeFrequency: 'yearly',
+      changeFrequency: 'daily' as const,
       priority: 1,
     },
-    {
-      url: 'https://itinerari-vivi.vercel.app/destinazioni',
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.8,
-    },
-  ]
+  ];
+
+  // Genera dinamicamente le rotte per le destinazioni
+  
+  const dynamicRoutes = ITINERARI_FULL.map((citta) => ({
+    url: `${baseUrl}/destinazioni/${citta.slug}`,
+    lastModified: new Date(),
+    changeFrequency: 'monthly' as const,
+    priority: 0.6,
+  }));
+
+  return [...staticRoutes, ...dynamicRoutes];
 }
