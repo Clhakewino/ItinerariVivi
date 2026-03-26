1f1b8f89c3404e3b44c534100445703c0a3c2e7b
Bruyin <chlacchy96@gmail.com>
2026-03-26 19:50:45 +0100

    creazione componente carousel

---
+++ app/components/CarouselTrips.tsx
@@ -0,0 +1,52 @@
+import Link from 'next/link';
+import { Itinerario } from '../data/itinerari';
+
+interface CarouselProps {
+  title: string;
+  subtitle: string;
+  itinerari: Itinerario[];
+}
+
+export default function CarouselTrips({ title, subtitle, itinerari }: CarouselProps) {
+
+    return (
+        // CAROSELLO
+        <section>
+            <header className="mb-12">
+                <h2 className="text-4xl font-bold">Itinerari più amati</h2>
+                <p className="text-slate-500 mt-2">Alcuni dei percorsi più ricercati</p>
+            </header>
+
+            {/*CONTENITORE: Carosello su mobile, Grid da md in su*/}
+            <div className="-mx-8 px-8 -mt-7 relative z-10 flex overflow-x-auto gap-4 pb-8 -mb-8 snap-x snap-mandatory scrollbar-hide md:mx-0 md:px-0 md:mb-0 md:pb-0 md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-8">
+                {itinerari.map((trip) => (
+                    <div
+                        key={trip.id}
+                        className="group min-w-[80vw] md:min-w-0 overflow-hidden rounded-2xl bg-white shadow-md transition hover:shadow-xl snap-center"
+                    >
+                        {/* Immagine */}
+                        <div
+                            className="h-48 w-full bg-cover bg-center transition duration-500 group-hover:scale-110"
+                            style={{ backgroundImage: `url(${trip.immagine})` }}
+                        />
+
+                        {/* Contenuto */}
+                        <div className="p-6">
+                            <div className="flex justify-between items-center mb-2">
+                                <span className="text-xs font-bold uppercase text-rose-500">{trip.difficolta}</span>
+                                <span className="text-xs text-slate-400">{trip.durata}</span>
+                            </div>
+                            <h3 className="text-xl font-bold first-letter:uppercase">{trip.slug} - {trip.titolo}</h3>
+                            <Link
+                                href={`/destinazioni/${trip.slug}`}
+                                className="mt-4 block w-full py-2 border border-slate-200 rounded-lg text-sm font-semibold text-center hover:bg-slate-900 hover:text-white transition"
+                            >
+                                Vedi Dettagli
+                            </Link>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </section>
+    );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,9 @@
 
 import { useState, useEffect } from 'react';
 
-import Link from 'next/link';
-
-import "./globals.css";
 import { ITINERARI_FULL, Itinerario } from './data/itinerari';
+
+import CarouselTrips from './components/CarouselTrips';
 
 export default function HomePage() {
   // Definiamo lo stato per il termine di ricerca
@@ -21,7 +20,7 @@ export default function HomePage() {
         trip.slug.toLowerCase().includes(searchLower))
     }) : [];
 
-    const citiesToShowInFirstCarousel = [...ITINERARI_FULL].filter(trip => trip.homeCarousel.includes(1))
+  const citiesToShowInFirstCarousel = [...ITINERARI_FULL].filter(trip => trip.homeCarousel.includes(1))
 
   const [randomTrips, setRandomTrips] = useState<Itinerario[]>([]);
 
@@ -121,81 +120,21 @@ export default function HomePage() {
             <hr className="mt-12 border-slate-200" />
           </section>
         )}
+  
+        {/* CAROSELLO */}
+        <CarouselTrips
+          title="Itinerari più amati"
+          subtitle="Alcuni dei percorsi più ricercati"
+          itinerari={citiesToShowInFirstCarousel}
+        />
 
         {/* CAROSELLO */}
-        <header className="mb-12">
-          <h2 className="text-4xl font-bold">Itinerari più amati</h2>
-          <p className="text-slate-500 mt-2">Alcuni dei percorsi più ricercati</p>
-        </header>
-
-        {/* CONTENITORE: Carosello su mobile, Grid da md in su */}
-        <div className="-mx-8 px-8 -mt-7 relative z-10 flex overflow-x-auto gap-4 pb-8 -mb-8 snap-x snap-mandatory scrollbar-hide md:mx-0 md:px-0 md:mb-0 md:pb-0 md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-8">
-
-          {citiesToShowInFirstCarousel.map((trip) => (
-            <div
-              key={trip.id}
-              className="group min-w-[80vw] md:min-w-0 overflow-hidden rounded-2xl bg-white shadow-md transition hover:shadow-xl snap-center"
-            >
-              {/* Immagine */}
-              <div
-                className="h-48 w-full bg-cover bg-center transition duration-500 group-hover:scale-110"
-                style={{ backgroundImage: `url(${trip.immagine})` }}
-              />
-
-              {/* Contenuto */}
-              <div className="p-6">
-                <div className="flex justify-between items-center mb-2">
-                  <span className="text-xs font-bold uppercase text-rose-500">{trip.difficolta}</span>
-                  <span className="text-xs text-slate-400">{trip.durata}</span>
-                </div>
-                <h3 className="text-xl font-bold first-letter:uppercase">{trip.slug} - {trip.titolo}</h3>
-                <Link
-                  href={`/destinazioni/${trip.slug}`}
-                  className="mt-4 block w-full py-2 border border-slate-200 rounded-lg text-sm font-semibold text-center hover:bg-slate-900 hover:text-white transition"
-                >
-                  Vedi Dettagli
-                </Link>
-              </div>
-            </div>
-          ))}
-        </div>
-
-        {/* CAROSELLO */}
-        <header className="mb-12">
-          <h2 className="text-4xl font-bold mt-12">Lasciati ispirare</h2>
-          <p className="text-slate-500 mt-2">Il viaggio perfetto che non avevi ancora pianificato</p>
-        </header>
-
-        {/* CONTENITORE: Carosello su mobile, Grid da md in su */}
-        <div className="-mx-8 px-8 -mt-7 relative z-10 flex overflow-x-auto gap-4 pb-8 -mb-8 snap-x snap-mandatory scrollbar-hide md:mx-0 md:px-0 md:mb-0 md:pb-0 md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-8">
-
-          {randomTripsToDisplay.map((trip) => (
-            <div
-              key={trip.id}
-              className="group min-w-[80vw] md:min-w-0 overflow-hidden rounded-2xl bg-white shadow-md transition hover:shadow-xl snap-center"
-            >
-              {/* Immagine */}
-              <div
-                className="h-48 w-full bg-cover bg-center transition duration-500 group-hover:scale-110"
-                style={{ backgroundImage: `url(${trip.immagine})` }}
-              />
-
-              {/* Contenuto */}
-              <div className="p-6">
-                <div className="flex justify-between items-center mb-2">
-                  <span className="text-xs font-bold uppercase text-rose-500">{trip.difficolta}</span>
-                  <span className="text-xs text-slate-400">{trip.durata}</span>
-                </div>
-                <h3 className="text-xl font-bold first-letter:uppercase">{trip.slug} - {trip.titolo}</h3>
-                <Link
-                  href={`/destinazioni/${trip.slug}`}
-                  className="mt-4 block w-full py-2 border border-slate-200 rounded-lg text-sm font-semibold text-center hover:bg-slate-900 hover:text-white transition"
-                >
-                  Vedi Dettagli
-                </Link>
-              </div>
-            </div>
-          ))}
+        <div className="my-12">
+        <CarouselTrips
+          title="Lasciati ispirare"
+          subtitle="Il viaggio perfetto che non avevi ancora pianificato"
+          itinerari={randomTripsToDisplay}
+        />
         </div>
       </main>
     </div>
