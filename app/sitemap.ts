b347e933c290b44e6842cbd5c6c56677749ab47a
Bruyin <chlacchy96@gmail.com>
2026-04-13 23:19:59 +0200

    ritorno a vecchia sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,5 +1,5 @@
 import { MetadataRoute } from 'next'
-import { client } from '../sanity/sanityClient'
+import { client } from './sanity/sanityClient'
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const baseUrl = 'https://itinerari-vivi.vercel.app'
